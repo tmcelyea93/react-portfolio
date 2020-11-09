c262a66c59b4885b7502501fe8c46d6ca7ea96a5
tmcelyea93 <tarahmcelyea@gmail.com>
2020-11-09 13:52:40 -0600

    added FrontPage

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import "./App.css";
 
 import NavBar from "./components/NavBar";
-import ContactPage from "./components/Pages/ContactPage";
+import FrontPage from "./components/Pages/FrontPage";
 import AboutPage from "./components/Pages/AboutPage";
+import ContactPage from "./components/Pages/ContactPage";
 import ProjectsPage from "./components/Pages/ProjectsPage";
 import theme from "./themes/theme";
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavBar />
+      <FrontPage />
       <AboutPage />
       <ProjectsPage />
       <ContactPage />
